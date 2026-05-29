01683435d99ba31fc49500ef50e91edc6d82c151
MuhammadYusufFaturahman-55 <2406002@itg.ac.id>
2026-05-29 10:06:10 +0700

    Update index.php

--- stockbarang/index.php
+++ stockbarang/index.php
@@ -112,7 +112,7 @@ require 'cek.php';
                                                     <!-- Modal Header -->
                                                     <div class="modal-header">
                                                     <h4 class="modal-title">Edit Barang</h4>
-                                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
                                                     </div>
 
                                                     <!-- Modal body -->
@@ -139,7 +139,7 @@ require 'cek.php';
                                                     <!-- Modal Header -->
                                                     <div class="modal-header">
                                                     <h4 class="modal-title">Hapus Barang?</h4>
-                                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
                                                     </div>
 
                                                     <!-- Modal body -->
